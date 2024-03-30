acba4522a6d7fb48b96942d044af6791ad1d19e3
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-03-30 15:10:53 +0300

    add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-var favbutton=document.getElementsByClassName('favbutton')[0];
-favbutton.addEventListener('click',()=>{
+var favbutton=document.getElementsByClassName('favbutton');
+function fav(index){
+favbutton[index].addEventListener('click',()=>{
 
-    alert('Fovrited!');
+  
+    console.log(index)
+    favbutton[index].children[0]
+    var fill=document.getElementsByClassName('bi-heart-fill')[index];
+    if(fill.style.display=='none'){
+        fill.style.display='block'
+    }
+    else{
+        fill.style.display=='none'
+    }
 })
+}
