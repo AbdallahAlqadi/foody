b02135817c3eb4517095c049fad3cd05e40aba96
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-04-04 21:03:51 +0300

    add

--- js/register.js
+++ js/register.js
@@ -1,11 +1,17 @@
 var form=document.getElementById('form');
-console.log(form)
-var firstname=document.getElementsByClassName('firstname');
-var lastname=document.getElementsByClassName('lastname');
-var email=document.getElementsByClassName('email');
-var password=document.getElementsByClassName('password');
+var firstname=document.getElementById('form3Example1');
+var lastname=document.getElementById('form3Example2');
+var email=document.getElementById('form3Example3');
+var password=document.getElementById('form3Example4');
 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit',function(e) {
     e.preventDefault();
     alert(firstname.value);
+    var userData={
+firstname:firstname.value,
+ lastname:lastname.value ,
+email:email.value,
+password:password.value   }
+console.log(userData);
+localStorage.setItem('user Data Forml local',JSON.stringify(userData));
 })
