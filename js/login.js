46fcc74794b440d6caae4b98f54f154b21c7eef4
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-04-06 12:41:25 +0300

    add

--- js/login.js
+++ js/login.js
@@ -1,10 +1,9 @@
-alert('abdallah')
 var form=document.getElementById('formlogin');
 var email=document.getElementById('form2Example1');
 var password=document.getElementById('form2Example2');
 form.addEventListener('submit',function(e){
 e.preventDefault();
-var alluser=JSON.parse(localStorage.getItem('user formlalocal'));
+var alluser=JSON.parse(localStorage.getItem('user Data Forml local'));
 for(var i=0;i<alluser.length;i++){
     if(alluser[i].email==email.value&&alluser[i].password==password.value){
         console.log('email alredy exist');
