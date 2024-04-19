c6ee80d197e311a10aa23f68643b776ad27ccf18
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-04-19 18:07:11 +0300

    add

--- js/flip.js
+++ js/flip.js
@@ -4,6 +4,11 @@ var cardText=document.getElementsByClassName('card-text');
 var cardLink=document.getElementsByClassName('card-link');
 var faceOrback=document.getElementsByClassName('face');
 var flipform=document.getElementById('flipform');
+var title=document.getElementById('title');
+var text=document.getElementById('text')
+var atage=document.getElementById('atage')
+var selectedindex;
+
  function flipThecard(a){
 if(faceOrback[a].innerHTML=='face'){
    faceOrback[a].innerHTML='Back'
@@ -21,6 +26,24 @@ else {
 }
  }
 
-
+function selectedcard(index){
+  alert('you select the card of index : '+(index+1))
+  selectedindex=index;
+}
 
  
+flipform.addEventListener('submit',function(e){
+e.preventDefault()
+if(selectedindex!=null){
+
+  cardTitl[selectedindex].innerHTML=title.value
+  cardText[selectedindex].innerHTML=text.value
+cardLink[selectedindex].innerHTML=atage.value
+
+
+}
+else{
+  alert('please select a carde')
+}
+
+})
