a0dc42f6df115ae31e841bea54fa2398d40ea95a
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-04-26 13:22:41 +0300

    add

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ var email=document.getElementById('form2Example1');
 var password=document.getElementById('form2Example2');
 form.addEventListener('submit',function(e){
 e.preventDefault();
-var alluser=JSON.parse(localStorage.getItem('userDataFormllocal'));
+var alluser=JSON.parse(localStorage.getItem('Data'));
 for(var i=0;i<alluser.length;i++){
     if(alluser[i].email==email.value&&alluser[i].password==password.value){
         console.log('email alredy exist');
@@ -11,8 +11,7 @@ for(var i=0;i<alluser.length;i++){
         window.location.href='index.html';
     }
 else{
-    console.log('email not exist');
-alert('email not exist')
+    
 }
 }
 })
